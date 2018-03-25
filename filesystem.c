70b49a5253f57e46acebb4ead1a83c24dfdb3981
francolmenar <correofalso97@hotmail.es>
2018-03-25 21:56:23 +0200

    Comentado un poco
    Nada importante

--- filesystem.c
+++ filesystem.c
@@ -14,6 +14,13 @@
 
 /*
  * @brief 	Generates the proper file system structure in a storage device, as designed by the student.
+ *
+ * NF4 The file system block size will be 2048 bytes.
+ * NF6 The file system will be used on disks from 50 KiB to 10 MiB.
+ * NF7 The size of on-disk filesystem metadata shall be minimized. (mierda)
+ * NF8 The implementation shall not waste available resources. (mierda)
+ *
+ * @param deviceSize: size of the disk to be formatted in bytes.
  * @return 	0 if success, -1 otherwise.
  */
 int mkFS(long deviceSize)
@@ -23,6 +30,9 @@ int mkFS(long deviceSize)
 
 /*
  * @brief 	Mounts a file system in the simulated device.
+ *
+ * NF5 Metadata shall persist between unmount and mount operations.
+ *
  * @return 	0 if success, -1 otherwise.
  */
 int mountFS(void)
@@ -32,6 +42,9 @@ int mountFS(void)
 
 /*
  * @brief 	Unmounts the file system from the simulated device.
+ *
+ * NF5 Metadata shall persist between unmount and mount operations.
+ *
  * @return 	0 if success, -1 otherwise.
  */
 int unmountFS(void)
@@ -41,6 +54,12 @@ int unmountFS(void)
 
 /*
  * @brief	Creates a new file, provided it it doesn't exist in the file system.
+ *
+ * NF1 The maximum number of files in the file system will never be higher than 40.
+ * NF2 The maximum length of the file name will be 32 characters.
+ * NF3 The maximum size of the file will be 1 MiB.
+ *
+ * @param fileName: name of the file to be created.
  * @return	0 if success, -1 if the file already exists, -2 in case of error.
  */
 int createFile(char *fileName)
@@ -50,6 +69,7 @@ int createFile(char *fileName)
 
 /*
  * @brief	Deletes a file, provided it exists in the file system.
+ * @param fileName: name of the file to be removed.
  * @return	0 if success, -1 if the file does not exist, -2 in case of error..
  */
 int removeFile(char *fileName)
@@ -58,7 +78,12 @@ int removeFile(char *fileName)
 }
 
 /*
- * @brief	Opens an existing file.
+ * @brief	Opens an existing file and initializes its seek pointer to the beginning of the file.
+ *
+ * F2 Every time a file is opened, its seek pointer will be reset to the beginning of the file.
+ * F5 File integrity must be checked, at least, on open operations.
+ *
+ * @param fileName: name of the file to be opened.
  * @return	The file descriptor if possible, -1 if file does not exist, -2 in case of error..
  */
 int openFile(char *fileName)
@@ -68,6 +93,7 @@ int openFile(char *fileName)
 
 /*
  * @brief	Closes a file.
+ * @param fileName: name of the file to be created.
  * @return	0 if success, -1 otherwise.
  */
 int closeFile(int fileDescriptor)
@@ -76,7 +102,15 @@ int closeFile(int fileDescriptor)
 }
 
 /*
- * @brief	Reads a number of bytes from a file and stores them in a buffer.
+ * @brief	Reads a number of bytes from a file, starting from the seek pointer of the file, and stores them in a buffer.
+ * The seek pointer of the file is incremented as many bytes read from the file.
+ *
+ * F6 The whole contents of a file could be read by means of several read operations.
+ *
+ * @param fileDescriptor: file descriptor of the file to be read.
+ * @param buffer: buffer that will store the read data after the execution of the function.
+ * @param numBytes: number of bytes to read from the file.
+ *
  * @return	Number of bytes properly read, -1 in case of error.
  */
 int readFile(int fileDescriptor, void *buffer, int numBytes)
@@ -86,6 +120,20 @@ int readFile(int fileDescriptor, void *buffer, int numBytes)
 
 /*
  * @brief	Writes a number of bytes from a buffer and into a file.
+ * The seek pointer of the file is incremented as many bytes written from the file.
+ *
+ * In case the operation exceeds the number of data blocks initially reserved for the file,
+ * new data blocks shall be reserved without violating the filesystem limits.
+ *
+ * F3 Metadata shall be updated after any write operation in order to properly reflect any modification in the file system.
+ * F7 A file could be modified by means of write operations
+ * F8 As part of a write operation, file capacity may be extended by means of additional data blocks.
+ * NF3 The maximum size of the file will be 1 MiB.
+ *
+ * @param fileDescriptor: file descriptor of the file to write into.
+ * @param buffer: data to be written.
+ * @param numBytes: number of bytes to write to the file from the buffer.
+ *
  * @return	Number of bytes properly written, -1 in case of error.
  */
 int writeFile(int fileDescriptor, void *buffer, int numBytes)
@@ -95,8 +143,15 @@ int writeFile(int fileDescriptor, void *buffer, int numBytes)
 
 
 /*
- * @brief	Modifies the position of the seek pointer of a file.
- * @return	0 if succes, -1 otherwise.
+ * @brief	Modifies the position of the seek pointer of a file according to a given reference and offset.
+ *
+ * @param fileDescriptor: file descriptor.
+ * @param whence: Constant value acting as reference for the seek operation. This could be: FS SEEK CUR, FS SEEK BEGIN or FSSEEKEND.
+ * @param offset: Number of bytes to displace the seek pointer from the FS SEEK CUR position.
+ * This value can be either positive or negative, although it will never allow positioning the seek pointer outside the limits of the file.
+ * If whence is set to FS SEEK BEGIN or FS SEEK END, the file pointer must be set to this position, regardless of the offset value.
+ *
+ * @return	0 if success, -1 otherwise.
  */
 int lseekFile(int fileDescriptor, long offset, int whence)
 {
