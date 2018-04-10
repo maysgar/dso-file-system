40a17cbbd4f7218047c3636619eed17b69de7b9e
francolmenar <100346094@alumnos.uc3m.es>
2018-04-10 20:49:54 +0200

    Lo del gabo sin que reviente
    He cambiado un error que pusieron los payos cuando subieron el código inicial que mandó un correo.

--- include/crc.h
+++ include/crc.h
@@ -1,9 +1,9 @@
 /*
- * OPERATING SYSTEMS DESING - 16/17
+ * OPERATING SYSTEMS DESING - 17/18
  *
  * @file 	crc.h
  * @brief 	Headers for the CRC functionality.
- * @date	01/03/2017
+ * @date	04/03/2018
  *
  * DO NOT MODIFY
  */
@@ -18,18 +18,20 @@
  *
  * @param	<buffer> to compute the CRC on.
  * @param	<length> of the buffer, in bytes.
+ * @param	<prev_crc> CRC of the previous block.
  * @return	A 16-bit unsigned integer containing the resulting CRC.
  */
-uint16_t CRC16(const unsigned char* buffer, unsigned int length);
+uint16_t CRC16(const unsigned char* buffer, unsigned int length, uint16_t prev_crc);
 
 /*
  * @brief	CRC32 implementation.
  *
  * @param	<buffer> to compute the CRC on.
  * @param	<length> of the buffer, in bytes.
+ * @param	<prev_crc> CRC of the previous block.
  * @return	A 32-bit unsigned integer containing the resulting CRC.
  */
-uint32_t CRC32(const unsigned char* buffer, unsigned int length);
+uint32_t CRC32(const unsigned char* buffer, unsigned int length, uint32_t prev_crc);
 
 /*
  * @brief	CRC64 implementation.
