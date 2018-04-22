3dc8eaedae6e5ab9a458e1cda4a41241ccceb5ab
Ismael Garrido Mansoa <100346215@alumnos.uc3m.es>
2018-04-22 16:48:44 +0200

    Add files via upload

--- include/metadata.h
+++ include/metadata.h
@@ -9,13 +9,19 @@
 #define INODE_MAX_NUMBER 40         /* Maximum number of i-nodes in the device */
 #define MAX_SIZE_FILE 1024 * 1024   /* Maximum file size in bytes */
 #define NAME_MAX 32                 /* NF2 The maximum length of the file name will be 32 characters */
+#define PADDING_SUPERBLOCK 999      /* Padding size for the superblock */
+#define PADDING_INODE 999           /* Padding size for the inode */
 #define MAX_BLOCK_PER_FILE 512      /* Maximum number of blocks per file */
-#define IMAP_SIZE (INODE_MAX_NUMBER / 8) /* Maximum number of imap entries */
-#define BMAP_SIZE ( (((MAX_FILE_SYSTEM_SIZE) / (SIZE_OF_BLOCK)) -1)  / 8) /* Maximum number of bmap entries */
+#define IMAP_SIZE INODE_MAX_NUMBER / 8 /* Maximum number of imap entries */
+#define BMAP_SIZE ( ((MAX_FILE_SYSTEM_SIZE) / (SIZE_OF_BLOCK)) -1)  / 8 /* Maximum number of bmap entries */
+
 
 /* Variables used in mkFS for validating the size of the device */
 #define MIN_FILE_SYSTEM_SIZE 50 * 1024    /* Minimum file system size */
 #define MAX_FILE_SYSTEM_SIZE 10 * 1024 * 1024 /* Maximum file system size */
+
+#define INODE_SIZE NAME_MAX + 8 /* Size of an inode in bytes */
+#define INODE_PER_BLOCK (int) ( (SIZE_OF_BLOCK) / (INODE_SIZE)) /* Amount of inodes which fit in a block */
 
 #define bitmap_getbit(bitmap_, i_) (bitmap_[i_ >> 3] & (1 << (i_ & 0x07)))
 static inline void bitmap_setbit(char *bitmap_, int i_, int val_) {
@@ -25,59 +31,31 @@ static inline void bitmap_setbit(char *bitmap_, int i_, int val_) {
     bitmap_[(i_ >> 3)] &= ~(1 << (i_ & 0x07));
 }
 
-/**  Unsigned Short range: 0 to 65,535
- *   Unsigned Int range: 0 to 4,294,967,295
- */
-
-/*
- * Size of superblock_t:
- * shorts: 5
- * Ints: 2
- * Chars: IMAP_SIZE + BMAP_SIZE
- */
-#define SUPERBLOCK_SIZE (5 * 2) + (2 * 4) + (IMAP_SIZE) + (BMAP_SIZE)
-#define SUPERBLOCK_PADDING (SIZE_OF_BLOCK) - (SUPERBLOCK_SIZE) /* Padding size for the superblock */
-
 typedef struct{
-    unsigned short magicNum;              /* Magic number of the superblock */
-    unsigned short numInodes;             /* Number of i-nodes in the device */
-    unsigned short firstInode;            /* Number of the 1st i-node in the device */
-    unsigned int dataBlockNum;            /* Number of data blocks in the device */
-    unsigned short firstDataBlock;        /* Number of the 1st data block */
-    unsigned int deviceSize;              /* Total disk space in bytes */
-    unsigned short inodesBlocks;          /* Number of blocks for the inodes */
-    char i_map [IMAP_SIZE];               /* inode map */
-    char b_map [BMAP_SIZE];               /* block map */
-    char padding[SUPERBLOCK_PADDING];     /* Padding field for fulfilling a block */
+    unsigned int magicNum;              /* Magic number of the superblock */
+    unsigned int numInodes;             /* Number of i-nodes in the device */
+    unsigned int firstInode;            /* Number of the 1st i-node in the device */
+    unsigned int dataBlockNum;          /* Number of data blocks in the device */
+    unsigned int firstDataBlock;        /* Number of the 1st data block */
+    unsigned int deviceSize;            /* Total disk space in bytes */
+    unsigned int inodesBlocks;          /* Number of blocks for the inodes */
+    char i_map [IMAP_SIZE];             /* inode map */
+    char b_map [BMAP_SIZE];             /* block map */
+    char padding[PADDING_SUPERBLOCK];   /* Padding field for fulfilling a block */
 } superblock_t;
-
-/*
- * Size of inode_t:
- * shorts: 1
- * Ints: 3
- * Chars: NAME_MAX
- */
-  #define INODE_SIZE (1 * 2) + (3 * 4) + (NAME_MAX)  /* Size of an inode in bytes */
 
 typedef struct{
     char name[NAME_MAX];                /* file name */
     unsigned int size;                  /* Current file size in Bytes */
     unsigned int directBlock;           /* Direct block number */
-    unsigned short opened;              /* To know if a file is opened or closed */  /* Yo creo que se podría poder un char broooos */
-    unsigned int ptr;                   /* Gabo explicame que es esto por favor */
+    unsigned int opened;                /* Status of the file (0 -> closed, 1 -> opened)  */
+    unsigned int ptr;                   /*   */
+    unsigned int position;              /* Position of the pointer inside the file */
 } inode_t;
-
-/*
- * Size of inode_block_t:
- * INODE_PER_BLOCK * INODE_SIZE
- */
-#define INODE_PER_BLOCK (int) ( (SIZE_OF_BLOCK) / (INODE_SIZE)) /* Amount of inodes which fit in a block */
-#define INODE_BLOCK_SIZE (INODE_PER_BLOCK) * (INODE_SIZE) /* Size of inode_block_t */
-#define INODE_BLOCK_PADDING (SIZE_OF_BLOCK) - (INODE_BLOCK_SIZE) /* Padding size for the inode_block_t */
 
 typedef struct{
     inode_t inodeArray [INODE_PER_BLOCK]; /* Inode array */
-    char padding[INODE_BLOCK_PADDING];    /* Padding field for fulfilling a block */
+    char padding[PADDING_INODE];        /* Padding field for fulfilling a block */
 } inode_block_t;
 
 
