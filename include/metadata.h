f1c9282fb78fbe3c913172311028f95d8e97b61d
Gabriel García <100346140@alumnos.uc3m.es>
2018-04-23 21:57:06 +0200

    UPDATED: To auxiliary.h

--- include/metadata.h
+++ include/metadata.h
@@ -83,20 +83,3 @@ typedef struct{
     inode_t inodeArray [INODE_PER_BLOCK]; /* Inode array */
     char padding[INODE_BLOCK_PADDING];    /* Padding field for fulfilling a block */
 } inode_block_t;
-
-
-int umount (void); /* write the default File System into the disk */
-int syncFS(void); /* writes the metadata into the disk */
-
-int needed_blocks(int bits, char type);
-int blocks_toWrite();
-int getInodePosition(char *fileName);
-int ialloc (void);
-int alloc (void);
-int ifree (int inode_id);
-int bfree (int block_id);
-int bmap(int inode_position, int offset);
-int syncSP();
-int syncIN();
-int blocks_toWrite(int bytesToWrite, int fileSize, int blockSize);
-
