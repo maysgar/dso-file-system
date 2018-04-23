b0a083b8ce1d92254f26821588d5b9e5fe9240df
Gabriel García <100346140@alumnos.uc3m.es>
2018-04-23 21:57:29 +0200

    ADDED: All methods

--- include/auxiliary.h
+++ include/auxiliary.h
@@ -5,3 +5,17 @@
  * @brief 	Headers for the auxiliary functions required by filesystem.c.
  * @date	01/03/2017
  */
+int umount (void); /* write the default File System into the disk */
+int syncFS(void); /* writes the metadata into the disk */
+
+int needed_blocks(int bits, char type);
+int blocks_toWrite();
+int getInodePosition(char *fileName);
+int ialloc (void);
+int alloc (void);
+int ifree (int inode_id);
+int bfree (int block_id);
+int bmap(int inode_position, int offset);
+int syncSP();
+int syncIN();
+int blocks_toWrite(int bytesToWrite, int fileSize, int blockSize);
